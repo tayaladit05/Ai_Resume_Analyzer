b79465f61802f5ca69cad9780214a541a47610eb
Adit Tayal <tayaladit5@gmail.com>
2026-03-26 19:29:14 +0530

    dependency resolution

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,11 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
+const PORT = process.env.PORT || 5000
+
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
